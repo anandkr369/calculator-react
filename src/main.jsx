99bea84b32ee357f4417dc923d7c17182349a167
Anand Kumar <anandkr369@gmail.com>
2024-03-18 17:49:56 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Stopwatch from './Stopwatch.jsx'
+import Calculator from './Calculator.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Calculator/>
+    <Stopwatch/> <br/><br/> <br/> <br/> 
+    <App /><br/> <br/> <br/> <br/> <br/> <br/> <br/><br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
+  </React.StrictMode>,
+)
